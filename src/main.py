0f65618b47d32c783081b6898971716b3847a372
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-14 12:38:26 +0300

    imported model from torch hub

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from websocket import WebSocketApp
+import torch
+
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
 def connect_to_websocket():
     ws = WebSocketApp("ws://localhost:8080")
