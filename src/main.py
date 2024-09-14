f2b238415b72797fff04877de45863d39adb879f
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-14 12:49:35 +0300

    converted frame to RGB to be accepted by YOLO model

--- src/main.py
+++ src/main.py
@@ -11,6 +11,9 @@ def process_frame(frame_data):
         # Decode the binary data into an image
         np_arr = np.frombuffer(frame_data, dtype=np.uint8)
         frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+        # Convert BGR image to RGB as YOLO expects RGB
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     except Exception as e:
         print(f"Error processing frame: {e}")
         return None
