04e2664eb0bf0b9dd854e85049ff8edd0068ff68
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-14 12:37:08 +0300

    started connection with websocket server

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
+from websocket import WebSocketApp
+
 def connect_to_websocket():
-    pass
+    ws = WebSocketApp("ws://localhost:8080")
+    ws.run_forever()
 
 if __name__ == "__main__":
     connect_to_websocket()
