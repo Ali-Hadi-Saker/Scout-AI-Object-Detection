e521d9f3472691b3fd7d61ccc55c5b91d7d4d407
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-14 12:43:25 +0300

    initiated process_frame function

--- src/main.py
+++ src/main.py
@@ -3,9 +3,14 @@ import torch
 
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
+
+def process_frame(frame_data):
+    pass
+
 def on_message(ws, message):
     try:
         print("Received frame from WebSocket server")
+        process_frame(message)
     except Exception as e:
         print(f"Error in message handling: {e}")
 
